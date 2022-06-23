c1cbc889b6a14c7dfba3838961f3458a3f4a7063
damien berthe <d.berthe1997@laposte.net>
2022-06-23 14:57:29 +0200

    23/06/22

--- SubApp/Weather/Weather.js
+++ SubApp/Weather/Weather.js
@@ -1,64 +1,91 @@
-import { Text, View, StyleSheet, PermissionsAndroid } from 'react-native'
-import React, { Component } from 'react'
+import {Text, View, StyleSheet, PermissionsAndroid} from 'react-native';
+import React, {Component} from 'react';
 import Geolocation from 'react-native-geolocation-service';
-  
+import {keyWeather} from './utils';
+
 export class Weather extends Component {
-
-state={
-  lat: null,
-  lon: null,
-};
-
-componentDidMount(){
-  this.getLocation();
-}
-  getLocation= async ()=> {
-
-const grandted= await PermissionsAndroid.request(
-PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-);
-
-if (grandted==PermissionsAndroid.RESULTS.GRANDTED) {
-  Geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-      this.setState({
-        lat: position.coords.latitude,
-        lan: position.coords.longitude,
-      })
-    },
-    (error) => {
-      // See error code charts below.
-      console.log(error.code, error.message);
-    },
-    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-);
-}
-
+  state = {
+    lat: null,
+    lon: null,
+    data: null,
   };
 
-getWeather=async ()=>{
-if (this.state.lat!=null) {
-  const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`)
-}
-};
+  componentDidMount() {
+    this.getLocation();
+  }
+  getLocation = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (granted == PermissionsAndroid.RESULTS.GRANTED) {
+      Geolocation.getCurrentPosition(
+          (position) => {
+          console.log(position);
+          this.setState({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          });
+          this.getWeather();
+        },
+        (error) => {
+          // See error code charts below.
+          console.log(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+      );
+    }
+  };
+
+
+
+  getWeather = async () => {
+    if (this.state.lat != null) {
+
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.lon}&appid=${keyWeather.apiKey}`,
+      );
+
+
+      const data = await response.json();
+      this.setState({
+        data: data,
+      });
+    }
+  };
 
   render() {
-    return (
-      <View 
-      style={{
-        flex:1,
-        backgroundColor: 'white',
-      }}>
-        <Text style={styles.title}>Weather</Text>
-      </View>
-    )
+const {data} = this.state;
+
+
+    if (data != null) {
+      return (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'white',
+          }}>
+          <Text style={styles.title}>Weather</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'green',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+          <Text style={styles.title}>Chargement ...</Text>
+        </View>
+      );
+    }
   }
 }
 
-
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 50,
-    },
+  title: {
+    fontSize: 50,
+  },
 });
---
+++ SubApp/Weather/utils.js
@@ -0,0 +1,3 @@
+export const keyWeather={
+    apiKey: 'b527c50a927ec677c7696fa1d09bcb8c',
+}
